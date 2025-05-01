1916a8a2540362864ef624e5d1bf488333972485
Michael Pumo <hello@michaelpumo.com>
2025-05-01 13:58:46 +0100

    Fade out.

--- components/app/Cover.vue
+++ components/app/Cover.vue
@@ -1,54 +1,49 @@
 <script lang="ts" setup>
 import IconLogoMark from '@/assets/icons/logo-mark.svg'
-// import { useScroll } from '@vueuse/core'
+import { useScroll } from '@vueuse/core'
+
+const mapRange = (value: number): number => Math.max(0, Math.min(1, value / 100))
+const reversePercentage = (percent: number): number => Math.max(0, Math.min(100, 100 - percent))
 
 const ready = ref(false)
+const { y } = useScroll(window)
 
-// const { x, y, isScrolling, arrivedState, directions } = useScroll(window)
+const stylesOut = computed(() => {
+  if (import.meta.server) {
+    return {
+      opacity: 100,
+    }
+  }
+
+  return {
+    opacity: mapRange(reversePercentage(100 / (window.innerHeight / 3) * y.value)),
+  }
+})
 
 onMounted(async () => {
-  await wait(500)
+  await wait(200)
   ready.value = true
 })
 </script>
 
 <template>
   <div
-    class="app-cover w-full h-dvh text-white pointer-events-none bg-black/20 opacity-0 transition-opacity duration-1000 ease-out"
+    class="app-cover w-full h-dvh text-white pointer-events-none transition-opacity duration-1000 ease-out"
     :class="{
-      'opacity-100': ready,
+      'opacity-0': !ready,
     }"
   >
     <div class="wrapper flex items-center justify-center h-full">
-      <IconLogoMark class="app-cover__logo" />
-
-      <!-- <pre>
-        {{ x }}
-        {{ y }}
-        {{ isScrolling }}
-        {{ arrivedState }}
-        {{ directions }}
-      </pre> -->
+      <IconLogoMark
+        class="w-[9.75vw] min-w-[100px] max-w-[160px] h-auto transition-transform duration-1000 ease-out"
+        :class="{
+          'rotate-10': !ready,
+        }"
+        :style="stylesOut"
+      />
     </div>
   </div>
 </template>
-
-<style scoped>
-@reference "@/assets/css/main.css";
-
-.app-cover {
-  /* transition: opacity 1.5s var(--ease-out); */
-}
-
-.app-cover__logo {
-  width: 9.75vw;
-  min-width: 100px;
-  max-width: 140px;
-  height: auto;
-  /* opacity: 0; */
-  /* animation: logo 2.5s 0.5s var(--ease-out) forwards; */
-}
-</style>
 
 <style lang="postcss">
 html.is-storyblok-editor .app-cover {
