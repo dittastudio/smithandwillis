906aa517b63d2e561ff5ce6a833b9879d77c2179
Michael Pumo <hello@michaelpumo.com>
2025-10-02 13:56:12 +0100

    Allow the composable to accept a generic so we can use any type. Infer it.

--- app/composables/useCentreAnchor.ts
+++ app/composables/useCentreAnchor.ts
@@ -1,8 +1,14 @@
-export const useCentreAnchor = (target: Ref<HTMLElement | undefined>, offset: number = 0) => {
-  const halfHeight = ref<number>(0)
+import type { ShallowRef } from 'vue'
+
+export const useCentreAnchor = <T extends HTMLElement = HTMLElement>(
+  target: Readonly<ShallowRef<T | null | undefined>>,
+  offset: number = 0,
+) => {
+  const halfHeight = ref(0)
 
   onMounted(async () => {
     await nextTick()
+
     if (target.value) {
       const { height } = target.value.getBoundingClientRect()
       halfHeight.value = Math.round(height / 2)
