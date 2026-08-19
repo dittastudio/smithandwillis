df405787f30cc4edfd57669149728813ff63d82f
Michael Pumo <hello@michaelpumo.com>
2026-08-19 15:14:55 +0100

    feat: enhance MediaGrid and TextLink components with improved structure

--- app/components/block/MediaGrid.vue
+++ app/components/block/MediaGrid.vue
@@ -12,17 +12,18 @@ const ratioDesktop = computed(() => ratioDimensions(block.ratio_desktop))
 </script>
 
 <template>
-  <div v-editable="block">
-    <ul
-      :class="['grid grid-cols-1 gap-2.5', {
-        'md:grid-cols-2': block.items.length === 2,
-        'md:grid-cols-3': block.items.length === 3,
-      }]"
+  <ul
+    v-editable="block"
+    :class="['grid grid-cols-1 gap-2.5', {
+      'md:grid-cols-2': block.items.length === 2,
+      'md:grid-cols-3': block.items.length === 3,
+    }]"
+  >
+    <li
+      v-for="item in block.items"
+      :key="item._uid"
     >
-      <li
-        v-for="item in block.items"
-        :key="item._uid"
-      >
+      <figure class="relative">
         <template v-if="isGridImageComponent(item)">
           <picture v-if="item.image_desktop?.filename">
             <MediaSource
@@ -54,7 +55,48 @@ const ratioDesktop = computed(() => ratioDimensions(block.ratio_desktop))
         <template v-else-if="isGridVideoComponent(item)">
           <pre>{{ item }}</pre>
         </template>
-      </li>
-    </ul>
-  </div>
+
+        <figcaption
+          v-if="item.title || item.sub_title"
+          class="absolute inset-0 flex flex-col items-start justify-end contain-layout contain-paint text-white"
+        >
+          <div class="sticky bottom-0">
+            <div class="block-image__gradient p-(--app-outer-gutter)">
+              <StoryblokLink
+                v-if="item.link?.cached_url"
+                :item="item.link"
+                class="block p-(--app-outer-gutter) -m-(--app-outer-gutter)"
+              >
+                <UiTextLink
+                  as="div"
+                  :is-external="item.link.linktype === 'url'"
+                >
+                  <h4 class="type-serif-medium-caps flex items-center gap-2">
+                    {{ item.title }}
+                  </h4>
+                </UiTextLink>
+
+                <p class="opacity-80 type-serif-medium transition-opacity duration-300 ease-out [a:hover_&]:opacity-70 italic">
+                  {{ item.sub_title }}
+                </p>
+              </StoryblokLink>
+
+              <div
+                v-else
+                class="flex flex-col"
+              >
+                <h4 class="type-serif-medium-caps flex items-center gap-2">
+                  {{ item.title }}
+                </h4>
+
+                <p class="opacity-80 type-serif-medium italic">
+                  {{ item.sub_title }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </li>
+  </ul>
 </template>
