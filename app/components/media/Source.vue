0a6245ac6fab6d1e63612e787a4174ba5c23fd4b
Michael Pumo <hello@michaelpumo.com>
2026-08-19 14:48:56 +0100

    refactor: simplify media prop type and optimize computed sizes logic

--- app/components/media/Source.vue
+++ app/components/media/Source.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 interface Props {
   src: string
-  media?: string | undefined
+  media?: string
   width: number
   height: number
   sizes?: string
@@ -10,23 +10,22 @@ interface Props {
 
 const { src, media, width, height, sizes, focus } = defineProps<Props>()
 
-const cSizes = computed(() => {
-  const img = useImage()
+const img = useImage()
 
-  return img.getSizes(src, {
-    provider: 'storyblok',
-    sizes: sizes || 'xs:100vw sm:100vw md:100vw lg:100vw xl:100vw',
-    modifiers: {
-      width,
-      height,
-      smart: true,
-      filters: {
-        format: 'webp',
-        ...(focus ? { focal: focus } : {}),
-      },
+const cSizes = computed(() => img.getSizes(src, {
+  provider: 'storyblok',
+  sizes: sizes || 'xs:100vw sm:100vw md:100vw lg:100vw xl:100vw',
+  modifiers: {
+    width,
+    height,
+    smart: true,
+    filters: {
+      format: 'webp',
+      ...(focus ? { focal: focus } : {}),
     },
-  })
-})
+  },
+}),
+)
 </script>
 
 <template>
