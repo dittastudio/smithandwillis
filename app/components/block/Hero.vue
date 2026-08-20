cea52e9f6b5ef4a43fe05b441dae353ad41124d3
Michael Pumo <hello@michaelpumo.com>
2026-08-20 15:15:05 +0100

    fix: improve image and video component handling in Hero.vue

--- app/components/block/Hero.vue
+++ app/components/block/Hero.vue
@@ -25,11 +25,9 @@ const { block } = defineProps<Props>()
           v-for="item in block.items"
           :key="item._uid"
         >
-          <picture
-            v-if="item.component === 'image' && item.image_desktop?.filename"
-          >
+          <picture v-if="isImageComponent(item) && item.image_desktop?.filename">
             <MediaSource
-              media="(orientation: landscape)"
+              :media="getMediaQuery('md')"
               :width="16"
               :height="9"
               :src="item.image_desktop.filename"
@@ -37,11 +35,10 @@ const { block } = defineProps<Props>()
             />
 
             <MediaSource
-              v-if="item.image_mobile?.filename"
-              media="(orientation: portrait)"
+              v-if="item.image_mobile?.filename || item.image_desktop?.filename"
               :width="10"
               :height="16"
-              :src="item.image_mobile.filename"
+              :src="item.image_mobile?.filename || item.image_desktop.filename"
               sizes="2xs:100vw xs:100vw sm:100vw"
             />
 
@@ -49,11 +46,47 @@ const { block } = defineProps<Props>()
               srcset=""
               class="size-full object-cover"
               :src="item.image_desktop.filename"
-              :alt="item.image_desktop.alt ?? ''"
+              :alt="item.image_mobile?.alt || item.image_desktop.alt || 'Smith & Willis'"
               loading="eager"
               preload
             />
           </picture>
+
+          <template v-else-if="isVideoComponent(item)">
+            <MediaVideo
+              v-if="item.video_mobile?.filename"
+              :class="[
+                'size-full object-cover',
+                { 'md:hidden': item.video_desktop?.filename },
+              ]"
+              class="size-full object-cover"
+              :sources="[
+                { src: item.video_mobile.filename },
+              ]"
+              playsinline
+              autoplay
+              muted
+              loop
+              lazy
+            />
+
+            <MediaVideo
+              v-if="item.video_desktop?.filename"
+              :class="[
+                'size-full object-cover',
+                { 'hidden md:block': item.video_mobile?.filename },
+              ]"
+              class="size-full object-cover"
+              :sources="[
+                { src: item.video_desktop.filename },
+              ]"
+              playsinline
+              autoplay
+              muted
+              loop
+              lazy
+            />
+          </template>
         </template>
       </UiParallax>
     </HeroScrollDown>
