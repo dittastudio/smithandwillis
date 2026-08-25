a9fb1ac26cabdde1444d87705add4519bdd1914e
Will The Dev <hello@willthe.dev>
2026-08-25 09:09:20 +0100

    feat: trial sentry set up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,35 +6,53 @@ const isSpa = process.env.IS_SPA === 'true'
 
 export default defineNuxtConfig({
   modules: [
-    ['@nuxt/eslint', {
-      config: {
-        standalone: false,
-        stylistic: true,
-        autoInit: false,
+    [
+      '@nuxt/eslint',
+      {
+        config: {
+          standalone: false,
+          stylistic: true,
+          autoInit: false,
+        },
       },
-    }],
-    ['@nuxt/image', {
-      provider: 'storyblok',
-      storyblok: {
-        baseURL: 'https://a2.storyblok.com',
+    ],
+    [
+      '@nuxt/image',
+      {
+        provider: 'storyblok',
+        storyblok: {
+          baseURL: 'https://a2.storyblok.com',
+        },
+        domains: ['storyblok.com', 'smithandwillis.london'],
+        quality: 80,
+        screens: {
+          '2xs': 375,
+          'xs': 480,
+          'sm': 600,
+          'md': 800,
+          'lg': 1200,
+          'xl': 1440,
+          '2xl': 1800,
+        },
       },
-      domains: ['storyblok.com', 'smithandwillis.london'],
-      quality: 80,
-      screens: {
-        '2xs': 375,
-        'xs': 480,
-        'sm': 600,
-        'md': 800,
-        'lg': 1200,
-        'xl': 1440,
-        '2xl': 1800,
-      },
-    }],
+    ],
     '@nuxtjs/seo',
     'nuxt-ai-ready',
-    ['nuxt-gtag', {
-      id: 'G-QPDWJKTYHH',
-    }],
+    [
+      'nuxt-gtag',
+      {
+        id: 'G-QPDWJKTYHH',
+      },
+    ],
+    [
+      '@sentry/nuxt/module',
+      {
+        org: 'ditta-60',
+        project: 'javascript-nuxt',
+        autoInjectServerSentry: 'top-level-import',
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    ],
     [
       '@storyblok/nuxt',
       {
@@ -42,8 +60,10 @@ export default defineNuxtConfig({
       },
     ],
   ],
+
   ssr: !isSpa,
   devtools: { enabled: true },
+
   app: {
     pageTransition: { name: 'fade', mode: 'out-in' },
     layoutTransition: false,
@@ -68,34 +88,51 @@ export default defineNuxtConfig({
       ],
     },
   },
+
   css: ['~/assets/css/app.css'],
+
   site: {
     url: 'https://smithandwillis.london',
     name: 'Smith & Willis',
   },
+
   runtimeConfig: {
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     public: {
       STORYBLOK_TOKEN: process.env.STORYBLOK_TOKEN,
       STORYBLOK_VERSION: process.env.STORYBLOK_VERSION,
+
+      sentry: {
+        dsn: process.env.NUXT_PUBLIC_SENTRY_DSN, // Use a public environment variable for the DSN
+      },
     },
   },
+
   alias: {
     '#storyblok-components': fileURLToPath(new URL('./.storyblok/types/294626585753511/storyblok-components', import.meta.url)),
     '#storyblok-types': fileURLToPath(new URL('./.storyblok/types/storyblok', import.meta.url)),
   },
+
   routeRules: {
     '/**': {
       prerender: !isSpa,
     },
   },
+
+  sourcemap: {
+    client: 'hidden',
+  },
+
   future: {
     compatibilityVersion: 4,
   },
+
   features: {
     noScripts: false,
   },
+
   compatibilityDate: '2025-04-13',
+
   nitro: {
     prerender: {
       crawlLinks: !isSpa,
@@ -106,6 +143,7 @@ export default defineNuxtConfig({
       ],
     },
   },
+
   vite: {
     plugins: [
       tailwindcss(),
@@ -120,14 +158,17 @@ export default defineNuxtConfig({
       },
     },
   },
+
   postcss: {
     plugins: {
       'postcss-nested': {},
     },
   },
+
   ogImage: {
     enabled: false,
   },
+
   sitemap: {
     excludeAppSources: true,
     sources: ['/api/sitemap'],
---
+++ sentry.client.config.ts
@@ -0,0 +1,21 @@
+import * as Sentry from '@sentry/nuxt'
+
+Sentry.init({
+  // If set up, you can use your runtime config here
+  // dsn: useRuntimeConfig().public.sentry.dsn,
+  dsn: useRuntimeConfig().public.sentry.dsn,
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nuxt/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
